17fa39e7c110a0fdb11f596daa7e98d844980a1b
Захар Бушуев <z.bushuev@isoit.ru>
2019-10-16 20:42:51 +0300

    Add project files.

---
+++ SignarRTestFeaches/Hubs/ChatHub.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignarRTestFeaches.Hubs
+{
+    public class ChatHub : Hub
+    {
+        // Этот метод могут дернуть любые пользователи которые подключились по WebSocket
+        public async Task SendMessage(string user, string message)
+        {
+            // Сервер дергает метод ReceiveMessage у всех подключенных клиентов с параметрами user, message
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
