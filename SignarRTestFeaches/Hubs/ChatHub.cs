d4a66e1c9be820bada20421d9320887e259fab06
Захар Бушуев <bushuevzi@mail.ru>
2019-10-16 23:28:45 +0300

    отключен https (андроид не доверяет сертификату разработчика)

--- SignarRTestFeaches/Hubs/ChatHub.cs
+++ SignarRTestFeaches/Hubs/ChatHub.cs
@@ -6,10 +6,10 @@ namespace SignarRTestFeaches.Hubs
     public class ChatHub : Hub
     {
         // Этот метод могут дернуть любые пользователи которые подключились по WebSocket
-        public async Task SendMessage(string user, string message)
+        public async Task Send(string user, string message)
         {
             // Сервер дергает метод ReceiveMessage у всех подключенных клиентов с параметрами user, message
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            await Clients.All.SendAsync("Receive", user, message);
         }
     }
 }
--- SignarRTestFeaches/Startup.cs
+++ SignarRTestFeaches/Startup.cs
@@ -42,7 +42,7 @@ namespace SignarRTestFeaches
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
